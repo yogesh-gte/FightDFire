c26e2de1d9f8ef301f6ff8f77cfad9e2708cb40b
kishor-gte <kishor@gtechnohubb.com>
2026-07-06 10:46:27 +0530

    errors solved

--- src/test/java/in/sp/main/FightthefireApplicationTests.java
+++ src/test/java/in/sp/main/FightthefireApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Fight D FearApplicationTests {
+class FightthefireApplicationTests {
 
 	@Test
 	void contextLoads() {
